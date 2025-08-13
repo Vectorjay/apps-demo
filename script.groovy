35859f8a6e4b4d67e0be104b7512c2dc5193df5d
Victor Ogbo Onyia <victor.onyia.o@gmail.com>
2025-08-13 13:48:47 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,1 +1,14 @@
+def buildApp(){
+    echo 'building the application...'
+}
 
+def tesApp(){
+    echo 'testing the application...'
+}
+
+def deploypp(){
+    echo 'deploying the application...'
+    echo "deploying version ${params.VERSION}"
+}
+
+return this
