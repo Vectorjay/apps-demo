9579e2bc79981a4c65b41f47d3549fc1f6994959
Victor Ogbo Onyia <victor.onyia.o@gmail.com>
2025-08-13 15:23:27 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,14 +1,21 @@
-def buildApp(){
+def buildJar(){
     echo 'building the application...'
+    sh 'mvn package'
 }
 
-def tesApp(){
-    echo 'testing the application...'
+def buildImage(){
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+        sh 'docker build -t vectorzy/demo-app:jma-3.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push vectorzy/demo-app:jma-3.0'
+    }
 }
 
-def deploypp(){
-    echo 'deploying the application...'
-    echo "deploying version ${params.VERSION}"
+def deployApp(){
+    echo "deploying the application..."
+    
 }
 
 return this
+
