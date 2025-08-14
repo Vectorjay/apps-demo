0054199f8c4850952388aedefbd1061acbaea62c
Victor Ogbo Onyia <victor.onyia.o@gmail.com>
2025-08-14 12:20:08 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,3 +1,13 @@
+def increment(){
+    echo 'incrementing app version...'
+    sh 'mvn build-helper:parse-version versions:set \
+        -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersio.nextIncrementVersion} versions:commit'
+    def matcher = readFile('pom.xml') =~ '<version>(.+)<version>'
+    def version = matcher [0] [1]
+    env.IMAGE_NAME = "$version-$BUILD_NUMBER"
+
+}
+
 def buildJar(){
     echo 'building the application...'
     sh 'mvn package'
@@ -6,9 +16,9 @@ def buildJar(){
 def buildImage(){
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){
-        sh 'docker build -t vectorzy/demo-app:jma-2.0 .'
+        sh "docker build -t vectorzy/demo-app:$IMAGE_NAME ."
         sh "echo \$PASS | docker login -u \$USER --password-stdin"
-        sh 'docker push vectorzy/demo-app:jma-2.0'
+        sh "docker push vectorzy/demo-app:$IMAGE_NAME"
     }
 }
 
