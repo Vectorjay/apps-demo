7cf5b87b2ad510cd08bc7c9c080801563afb3525
Victor Ogbo Onyia <victor.onyia.o@gmail.com>
2025-08-14 12:42:28 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,7 +1,7 @@
 def increment(){
     echo 'incrementing app version...'
     sh 'mvn build-helper:parse-version versions:set \
-        -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementVersion} versions:commit'
+        -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.nextminorVersion}.\\\${parsedVersion.incrementVersion} versions:commit'
     def matcher = readFile('pom.xml') =~ '<version>(.+)<version>'
     def version = matcher [0] [1]
     env.IMAGE_NAME = "$version-$BUILD_NUMBER"
